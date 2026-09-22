8f1f140385d63e80b7e9230f8bcdb95f3abec271
ahmedtamer2006 <ahmedtamerfoc2000@gmail.com>
2026-09-22 21:01:24 +0300

    Fix lecture PDF URL in data.js for accurate resource linking

--- js/data.js
+++ js/data.js
@@ -3819,7 +3819,7 @@ const MATERIALS = {
                                 num: 1,
                                 title: "",
                                 lectures: [
-                                    { id: 101, title: "Lect 1 Dr.Zatout 'Handwriting'", type: "pdf", url: "materials/Pdfs/Economics/First_Term/Chapter_1/week1/Lect 1 Dr.Zatout.pdf" },
+                                    { id: 101, title: "Lect 1 Dr.Zatout 'Handwriting'", type: "pdf", url: "materials/Pdfs/Economics/First_Term/Chapter_1/week1/lect_1_dr.zatout_'handwriting'.pdf" },
 
                                     // { id: 102, title: "", type: "video", url: "" }
                                 ]
